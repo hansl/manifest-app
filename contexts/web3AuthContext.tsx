cdc84b497bf0541879b875ef4a91fba222d1dc53
Hans Larsen <hans@larsen.online>
2025-02-12 10:44:51 -0800

    Remove MFA prompt in development mode

--- contexts/web3AuthContext.tsx
+++ contexts/web3AuthContext.tsx
@@ -80,7 +80,7 @@ export const Web3AuthProvider = ({ children }: { children: ReactNode }) => {
             logo: '/sms',
           },
         ],
-        mfaLevel: 'optional',
+        mfaLevel: env.production ? 'optional' : 'none',
         client: {
           clientId: env.web3AuthClientId,
           web3AuthNetwork: env.web3AuthNetwork as WEB3AUTH_NETWORK_TYPE, // Safe to cast since we validate the env vars in config/env.ts
